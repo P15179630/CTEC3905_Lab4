85bd6f5fa6c2479d69505bbad329df58889765a6
P15179630 <P15179630@my365.dmu.ac.uk>
2018-10-29 14:51:38 +0000

    menu now toggling on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menuNav = document.getElementById('menu-nav');
 
-const toggleMenu = () => { console.log("called toggleMenu")}
+const toggleMenu = () => { menuNav.classList.toggle("menu-toggle")}
 
 menuToggle.addEventListener('click', toggleMenu);
