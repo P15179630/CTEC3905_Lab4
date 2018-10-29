f30a89dcd4135f7bcc9ef8364f49731df57a2e6f
P15179630 <P15179630@my365.dmu.ac.uk>
2018-10-29 14:47:02 +0000

    added arrow function to log toggling menu when menu div clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+const toggleMenu = () => { console.log("called toggleMenu")}
+
+menuToggle.addEventListener('click', toggleMenu);
